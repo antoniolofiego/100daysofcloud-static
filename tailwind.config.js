6453040a30f43250df529447461bf2fad9dee81c
Antonio Lo Fiego <alofiego@hey.com>
2020-08-30 17:40:27 -0400

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    purge: [],
+    theme: {
+        fontFamily: {
+            sans: ['Montserrat'],
+            serif: ['Montserrat'],
+            mono: ['Montserrat'],
+            display: ['Montserrat'],
+            body: ['Montserrat'],
+        },
+        extend: {
+            maxWidth: {
+                1440: '1440px',
+            },
+        },
+    },
+    variants: {},
+    plugins: [],
+};
